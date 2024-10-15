b6e6defbcc850b5ef835d220ba6ec5fe415c1f46
Isaque Franklin <isaquefrankli@gmail.com>
2024-10-14 21:46:18 -0300

    First commit.

---
+++ toralize.c
@@ -0,0 +1,18 @@
+/* toralize.c */
+#include "toralize.h"
+
+int main(int argc, char *argv[]){
+    char *host;
+    int port;
+
+    int (argc < 3) {
+        fprintf(stderr, "Usage: $s <host> <port>\n",
+        argv[0]);
+        return -1;
+    };
+
+    host  = argv[1];
+    port = atoi(argv[2]);
+}
+
+// ./toralize 1.2.3.4 80
---
+++ toralize.h
@@ -0,0 +1,24 @@
+/* toralize.h */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROXY "127.0.0.1"
+#define PROXYPORT 9050
+
+typedef unsigned char  int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+struct proxy_request {
+  int8 vn;
+  int8 cd;
+  int16 dstport;
+  int32 dstip;
+  unsigned char userid[8];
+};
+
+struct proxy_response {
+
+}
